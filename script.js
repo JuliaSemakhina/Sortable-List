b7939e2b6df544659ff69c6186786b2af53ba2aa
Julia Semakhina <contremoi@yandex.ru>
2020-11-30 18:25:24 +0300

    final files

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const draggable_list = document.getElementById('draggable-list');
 const check = document.getElementById('check');
 
-const richestPeople = [
+const largestCountry = [
 	'Russia',
 	'Canada',
 	'United States',
@@ -24,12 +24,12 @@ createList();
 //Insert list items into DOM
 
 function createList() {
-	[...richestPeople]
+	[...largestCountry]
 	.map(a => ({ value: a, sort: Math.random()}))
 	.sort((a,b)=> a.sort - b.sort)
 	.map(a=> a.value)
-	.forEach((person, index)=> {
-		console.log(person);
+	.forEach((country, index)=> {
+		console.log(country);
 
 		const listItem= document.createElement('li');
 
@@ -38,7 +38,7 @@ function createList() {
 		listItem.innerHTML = `   
 			<span class="number">${index + 1}</span>
 			<div class="draggable" draggable="true">
-			<p class="person-name">${person}</p>
+			<p class="country-name">${country}</p>
 			<i class='fas fa-grip-lines'></i>
 			</div>
 		`;
@@ -50,23 +50,49 @@ function createList() {
 }
 
 function dragStart(){
-	console.log('Event: ', 'dragstart');
+	dragStartIndex = +this.closest('li').getAttribute('data-index');
 }
 
 function dragEnter(){
-	console.log('Event: ', 'dragenter');
+	this.classList.add('over');
 }
 
-function dragOver(){
-	console.log('Event: ', 'dragover');
+function dragOver(e){
+	e.preventDefault();
 }
 
 function dragLeave(){
-	console.log('Event: ', 'dragleave');
+	this.classList.remove('over');
 }
 
 function dragDrop(){
-	console.log('Event: ', 'drop');
+	const dragEndIndex = +this.getAttribute('data-index');
+	swapItems(dragStartIndex, dragEndIndex);
+	this.classList.remove('over');
+	console.log(dragEndIndex);
+}
+
+
+//Swap list items
+function swapItems(fromIndex, toIndex){
+	const itemOne = listItems[fromIndex].querySelector('.draggable');
+	const itemTwo = listItems[toIndex].querySelector('.draggable');
+	listItems[fromIndex].appendChild(itemTwo);
+	listItems[toIndex].appendChild(itemOne);
+}
+
+
+//Check the order of the list items on button click
+function checkOrder() {
+	listItems.forEach((listItem,index)=> {
+		const countryName = listItem.querySelector('.draggable').innerText.trim();
+		if(countryName !== largestCountry[index]){
+			listItem.classList.add('wrong');
+		} else {
+			listItem.classList.remove('wrong');
+			listItem.classList.add('right');
+		}
+	})
 }
 
 
@@ -85,3 +111,5 @@ function addeventListeners(){
 		item.addEventListener('dragleave', dragLeave);
 });
 }
+
+check.addEventListener('click', checkOrder);
